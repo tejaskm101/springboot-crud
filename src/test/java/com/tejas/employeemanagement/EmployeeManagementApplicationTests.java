2b35490b30972cffef3b67b0a6f48dafe76d5ac6
Tejas Kumar <tejaskm101@gmail.com>
2025-06-25 18:32:54 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/tejas/employeemanagement/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tejas.employeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
